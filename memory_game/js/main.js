789a6bd2292878b6e983b924ad1beed9ddcb3e40
John Hayden <johnhayden@Johns-MacBook-Air.local>
2019-05-23 12:28:39 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThee = "king";
-var cardFour = "king";
+cards = ['queen', 'queen', 'king', 'king'];
+cardsInPlay = [];
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+}
+}
