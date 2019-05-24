16af1298e53da1c27090bdd47e86f30dc7ec46a2
John Hayden <johnhayden@Johns-MacBook-Air.local>
2019-05-23 20:13:29 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-const cards = [
+cards = [
 {
 	rank: "queen",
 	suit: "hearts",
@@ -22,6 +22,16 @@ const cards = [
 ];
 cardsInPlay = [];
 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		document.getElementById('game-board').addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 function checkForMatch() {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			console.log("You found a match!");
@@ -30,13 +40,17 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-	checkForMatch();
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-}	
+}
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
